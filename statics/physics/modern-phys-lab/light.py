9149f32588e66f61802f3dafa2a7a398d45a12ca
Nathan Nguyen <159226326+Indigo5684@users.noreply.github.com>
2025-03-30 19:36:08 -0500

    Modern Lap Update

--- statics/physics/modern-phys-lab/light.py
+++ statics/physics/modern-phys-lab/light.py
@@ -1,12 +1,12 @@
 from uncertainties import ufloat
 from uncertainties.umath import * 
 import math
-# Units in CM
-D = ufloat(3.1, 0.15)
+# Units in M
+D = ufloat(3.10, 0.15)
 A = ufloat(0.26, 0.005)
 B = ufloat(0.452, 0.003)
-omega = ufloat(2000*2*math.pi, 0)
+o = 2039*2*math.pi
 r = ufloat(0.00156, 0.005)
-c = 4*(A * D**2 * omega)/(r * (D + B))
+c = (4*A * D**2 * (o))/(r * (D + B))
 
 print(c)
